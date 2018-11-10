96b9b71241f9a0cecc1cccf5fe926b4ccd50561e
Jack Gilmartin <s00171013@mail.itsligo.ie>
2018-11-10 15:35:52 +0000

    Refactored TestGruntController.
    Fixed a small bug where the enemy would investigate the same disturbance again each time they reached a patrol point.

--- Assets/Scripts/AI/TestGruntController.cs
+++ Assets/Scripts/AI/TestGruntController.cs
@@ -18,63 +18,60 @@ public class TestGruntController : MonoBehaviour
     public RaycastHit hit;
 
     public float haltTime;
+    private bool halted;
 
     TimeManager tm = new TimeManager();
-   
+
     void Update()
     {
-
         // https://unity3d.com/learn/tutorials/topics/navigation/basics?playlist=17105
         // Some of the basic code for this functionality was seen in Brackeys' "Unity NavMesh Tutorial - Basics" video.
         // I'm just using it as a test for the grunt's AI.
 
-        if(agent.isStopped == true)
-        {
-            Investigate();
-        }
+        if(halted) Halt();
 
         // If you click the mouse on-screen. This will be taken out soon, it's only for testing purposes.
         if (Input.GetMouseButtonDown(0))
         {
             // Send out a ray to the position where you clicked.
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);            
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
             // This if will determine whether or not you clicked an object.
             if (Physics.Raycast(ray, out hit))
             {
-                Halt();                                         
+                halted = true;               
             }
         }
     }
 
     void Halt()
     {
-        // The enemy stops in his tracks upon hearing a noise.
-        agent.isStopped = true;      
+        if (agent.isStopped == false)
+        {
+            // The enemy stops in his tracks upon hearing a noise.
+            agent.isStopped = true;
 
-        // I'd really like to get him to face the direction of the disturbance here. I still have to work it out.
+            // We'll hopefully have something like this appear in-game soon.
+            print("What was that?");
+        }
 
-        // We'll hopefully have something like this appear in-game soon.
-        print("What was that?");
+        // I'd really like to get him to turn and face the direction of the disturbance here. I still have to work it out.    
+
+        // The enemy will then move in to investigate.
+        if (tm.TimeCount(haltTime)) Investigate();
     }
 
     void Investigate()
-    {   
-        // Time count keeps track of the seconds that have passed.
-        //timeCount += Time.deltaTime;
+    {
+        // The agent can now move again.
+        halted = false;
+        agent.isStopped = false;
 
-        // If the enemy has been halted for the specified halt time...
-        if (tm.TimeCount(haltTime))
-        {                        
-            // The agent can now move again.
-            agent.isStopped = false;            
+        // If you clicked an object. The agent should head towards its' position.
+        // TODO: I should work out how to get the enemy to stop a few feet in front of where they're headed.
+        // https://docs.unity3d.com/Manual/DirectionDistanceFromOneObjectToAnother.html
 
-            // If you clicked an object. The agent should head towards its' position.
-            // TODO: I should work out how to get the enemy to stop a few feet in front of where they're headed.
-            // https://docs.unity3d.com/Manual/DirectionDistanceFromOneObjectToAnother.html
-
-            // The agent moves toward the source of the disturbance.
-            agent.SetDestination(hit.point);
-        }
+        // The agent moves toward the source of the disturbance.
+        agent.SetDestination(hit.point);
     }
 }
