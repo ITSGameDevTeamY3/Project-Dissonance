1c3f5723d3a08988165e2ec4d4e41aad9641cac2
S00168347 <fearonedm@outlook.com>
2018-11-19 15:51:31 +0000

    Footstep sound effects added to enemy. (FMOD needed for this!)

---
+++ Assets/Scripts/SFX/Footsteps.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Footsteps : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+
+    public string inputSound;
+    bool isMoving;
+    public float moveSpeed;
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        InvokeRepeating("GetFootSteps", 0, moveSpeed);
+    }
+
+    void Update()
+    {
+        //GetFootSteps();
+    }
+
+    void GetFootSteps()
+    {
+        if (!agent.isStopped)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(inputSound);
+        }
+    }
+
+    void OnDisable()
+    {
+        isMoving = false;
+    }
+}
