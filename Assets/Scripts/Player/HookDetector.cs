5d51d10a40902743aead25eb3605e725e5ba3815
s00172994 <s00172994@mail.itsligo.ie>
2018-12-06 02:47:05 +0000

    Update Donnacha Scene

---
+++ Assets/Scripts/Player/HookDetector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookDetector : MonoBehaviour {
+
+    public GameObject Player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hookable"))
+        {
+            Player.GetComponent<GrapplingHook>().IsHooked = true;
+            Player.GetComponent<GrapplingHook>().HookedObj = other.gameObject;
+        }
+    }
+}
