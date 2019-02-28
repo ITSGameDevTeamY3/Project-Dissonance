db76a59743ee259a00d20f64acff190b96027ac6
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-02-28 22:30:00 +0000

    Adrian's menu & FMOD scripts imported.
    The menu will need some tweaking. addons for TextMeshPro have been installed too.

---
+++ Assets/Scripts/SFX/AmbianceSoundEmitter.cs
@@ -0,0 +1,22 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using FMOD;
+//using FMODUnity;
+
+//public class AmbianceSoundEmitter : MonoBehaviour {
+
+//    [FMODUnity.EventRef]
+
+//    public string inputSound;
+
+//    // Use this for initialization
+//    void Start () {
+//        RuntimeManager.PlayOneShot(inputSound, transform.position);
+//    }
+	
+//	// Update is called once per frame
+//	void Update () {
+		
+//	}
+//}
---
+++ Assets/Scripts/SFX/UIPlay.cs
@@ -0,0 +1,25 @@
+﻿//using FMOD.Studio;
+//using FMODUnity;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class UIPlay : MonoBehaviour {
+
+//    EventInstance playSelectSound;
+
+//    void PlaySound()
+//    {
+//        playSelectSound = RuntimeManager.CreateInstance("event:/Master/SFX_Events/UI/Select");
+//    }
+
+//    // Use this for initialization
+//    void Start () {
+		
+//	}
+	
+//	// Update is called once per frame
+//	void Update () {
+		
+//	}
+//}
