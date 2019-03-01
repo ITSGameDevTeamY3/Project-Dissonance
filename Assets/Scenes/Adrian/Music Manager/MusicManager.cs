7ec2dd83de0f399d98478b6753e255872f1316d6
Adrian <fearonedm@outlook.com>
2019-03-01 01:45:11 +0000

    Adaptive Music Wip
    Code is fine although still working on the transitions on Fmod.

---
+++ Assets/Scenes/Adrian/Music Manager/MusicManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class MusicManager : MonoBehaviour {
+
+    public enum Conditions { Normal, Alerted, Spotted }
+
+    public Conditions condition = Conditions.Normal;
+
+    [EventRef]
+    public string music = "event:/Master/Music_Events/WorldMusic";
+    EventInstance musicEvnt;
+
+    // Use this for initialization
+    void Start() {
+        musicEvnt = RuntimeManager.CreateInstance(music);
+        
+        musicEvnt.start();
+    } 
+	
+    private void CheckMusicCondition()
+    {
+        switch (condition)
+        {
+            case Conditions.Normal:
+                musicEvnt.setParameterValue("alerted", 1f);
+                musicEvnt.setParameterValue("Spotted", 0f);
+                musicEvnt.setParameterValue("Normal", 0f);
+                break;
+            case Conditions.Alerted:
+                musicEvnt.setParameterValue("alerted", 0f);
+                musicEvnt.setParameterValue("Spotted", 0f);
+                musicEvnt.setParameterValue("Normal", 1f);
+                break;
+            case Conditions.Spotted:
+                musicEvnt.setParameterValue("alerted", 0f);
+                musicEvnt.setParameterValue("Spotted", 1f);
+                musicEvnt.setParameterValue("Normal", 0f);
+                break;
+            default:
+                musicEvnt.setParameterValue("Normal", 1f);
+                break;
+        }      
+    }
+
+    // Update is called once per frame
+    void Update () {
+        CheckMusicCondition();
+	}
+}
---
+++ Assets/Scenes/Adrian/SFX/Footsteps.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using FMOD.Studio;
+using FMODUnity;
+
+[RequireComponent(typeof(EnemyController))]
+public class Footsteps : MonoBehaviour
+{
+    [EventRef]
+
+    public string inputSound;
+    [EventRef]
+    public string AlertTheme;
+    bool isMoving;
+    public float moveSpeed;
+
+    NavMeshAgent agent;
+    EnemyController enemyController;
+    public bool alerted = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        enemyController = GetComponent<EnemyController>();
+        InvokeRepeating("GetFootSteps", 0, moveSpeed);
+
+        //if (enemyController.enemyPhase == EnemyController.Phase.ALERT)
+        //{
+        //    GetAttackingTheme();
+        //}
+    }
+    void Update()
+    {
+        //GetFootSteps();
+    }
+
+    void GetFootSteps()
+    {
+        if (enemyController.enemyPhase != EnemyController.Phase.HALT && !agent.isStopped)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(inputSound, transform.position);
+        }
+    }
+
+    //void GetAttackingTheme()
+    //{
+    //   RuntimeManager.PlayOneShot(AlertTheme);
+    //}
+
+    //public void GetPatrolingTheme()
+    //{
+    //    if (!alerted)
+    //    {
+    //        RuntimeManager.PlayOneShot(AlertTheme);
+    //        alerted = true;
+    //    }        
+    //}
+
+    //public void StopPatrolingTheme()
+    //{
+    //    if (alerted)
+    //    {
+    //        // Destroys ALL sounds, we want it to destroy only one.
+    //      //  RuntimeManager.Destroy();
+    //      //  alerted = false;
+    //    }
+    //}
+
+    void OnDisable()
+    {
+        isMoving = false;
+    }
+}
