e6ebdc9a94c38186ad7845cc8a9657bafc93987d
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-03-06 04:41:43 +0000

    GameController added.

---
+++ Assets/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController instance;
+    public GameObject Player;
+    public ThirdPersonCharacter ThirdPersonScript;
+
+    public int timeTaken, alerts;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+        ThirdPersonScript = Player.GetComponent<ThirdPersonCharacter>();
+    }
+
+    //void Start () {
+
+    //}
+
+    // Update is called once per frame
+    void Update () {
+        if (ThirdPersonScript.Health <= 0) EndGame();
+	}
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene("MissionFailed");
+
+        // Stop music and fade might be good here.
+    }
+}
